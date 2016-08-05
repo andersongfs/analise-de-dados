133fca1a41b36a3edbd8d6a263d2a1c00c16a4a5
Anderson Gustafson <andersongfs@MacBook-Air-de-Anderson.local>
2016-08-05 01:38:57 -0300

    Explicando a fonte dos dados e o gráfico

--- T2CP3/t2cp3/ui.R
+++ T2CP3/t2cp3/ui.R
@@ -15,12 +15,34 @@ library(plotly)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Análise de dados I"),
+  titlePanel("Análise de dados I - T2CP3"),
   
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotlyOutput("trendPlot")
+      h2("Sobre os dados", align = "center"),
+      p("Os dados que são a base da análise descrevem os gastos realizados pelos deputados brasileiros no ano de 2016. 
+        Estes dados são abertos e disponibilizados pela própria câmara legislativa e podem ser encontrados neste link: 
+        http://www2.camara.leg.br/transparencia/cota-para-exercicio-da-atividade-parlamentar/dados-abertos-cota-parlamentar", align = "justify"),
+      p("Para conhecer o que está descrito no conjunto de dados, basta acessar este outro link:
+        http://www2.camara.leg.br/transparencia/cota-para-exercicio-da-atividade-parlamentar/dados-abertos-cota-parlamentar", align = "justify"),
+      br(),
+      
+      h2("Análise", align = "center"),
+      p("O gráfico a seguir faz uma relação dos gastos de cada estado brasileiro pela sua quantidade de representantes. O mesmo possui duas variáveis quantitativas e duas categóricas, sendo elas respectivamente: a quantidade de deputados(mensurada pelo eixo x), a soma dos gastos(mensurada pelo eixo y), o estado em questão(marcado pelos pontos no gráfico) e as regiões do Brasil(mostrado usando cores e descritas na legenda à direita do gráfico). 
+        Com este gráfico temos como objetivo principal notar como é a função de crescimento dos gastos conforme o aumento da quantidade de deputados e, em segundo plano, queremos observar também se alguma região possui estados com forte semelhança nos índices de gasto e quantidade de deputados.", align = "justify"),
+      p("Então, como essa relação acontece na sua região? Qual desses pontos é seu estado? Os estados da sua região possuem valores de gasto e/ou quantidade de deputados parecidos?"),
+      p("Fique à vontade para ampliar e analisar melhor qualquer parte do gráfico!"),
+      br(),
+      
+      plotlyOutput("trendPlot"),
+      br(),
+      
+      p("Com a visualização podemos notar um crescimento contínuo dos gastos conforme aumenta a quantidade de deputados, seguindo uma linha em comum de crescimento. Portanto, os estados que mais possuem deputados são os que mais gastam. Além disso vemos uma forte concentração perto do ponto (0, 0), o que nos faz concluir que muitos estados possuem um número parecido de representantes. E, por fim, notamos uma concentração dos pontos azuis e pretos, indicando que os estados das regiões Norte e Centro-Oeste possuem valores aproximados de quantidade de deputados e de gasto total.", align = "justify"),
+      br(),
+      
+      h2("Interatividade do gráfico", align = "center"),
+      p("Como visto em sala de aula, podemos utilizar vários recursos para facilitar a visualização. Neste gráfico os recursos de interatividade utilizados foram: zoom, brush, hover e filtragem pelas regiões clicando nos nomes mostrados na legenda.", align = "justify")
     )
   )
 )
