b06b244b9b4fcd51265af750f27db37e4853560f
Anderson Gustafson <andersongfs@gmail.com>
2016-08-19 16:48:36 -0300

    Analise do agrupamento ok

---
+++ T3CP1/leitor.R
@@ -0,0 +1,27 @@
+ler_gastos <- function(arquivo = "ano-atual.csv"){
+  #' Lê um csv criado a partir dos dados de gastos dos deputados da 
+  #' Câmara e seta os tipos de colunas mais convenientemente. 
+  require("readr")
+  require("dplyr", warn.conflicts = FALSE)
+  
+  gastos = read_csv(arquivo, 
+                    col_types = list(
+                      datEmissao = col_datetime()
+                    ))
+  gastos = gastos %>% 
+    mutate_each(funs(as.factor), sgPartido, sgUF, txNomeParlamentar, indTipoDocumento)
+  return(gastos)
+} 
+
+ler_gastos2 <- function(arquivo = "ano-atual.csv"){
+  #' Lê um csv criado a partir dos dados de gastos dos deputados da 
+  #' Câmara e seta os tipos de colunas mais convenientemente. 
+  #' Versão sem readr, para máquinas onde não é possível instalar esse pacote. 
+  #' É um pouco mais lenta que a com readr.
+  require("dplyr", warn.conflicts = FALSE)
+  
+  gastos = read.csv(arquivo, stringsAsFactors = FALSE)
+  gastos = gastos %>% 
+    mutate_each(funs(as.factor), sgPartido, sgUF, txNomeParlamentar, indTipoDocumento)
+  return(gastos)
+} 
